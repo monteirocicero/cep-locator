03baa84aacea6d7f3a8d8883494b597f66a2ef84
Cicero Monteiro <cicerolmonteiro@gmail.com>
2016-05-21 18:39:23 -0300

    Initial commit

---
+++ src/main/java/com/cicero/demo/entity/Greeting.java
@@ -0,0 +1,22 @@
+package com.cicero.demo.entity;
+
+public class Greeting {
+	
+	private final long id;
+	private final String content;
+	
+	public Greeting(long id, String content) {
+		super();
+		this.id = id;
+		this.content = content;
+	}
+
+	public long getId() {
+		return id;
+	}
+	
+	public String getContent() {
+		return content;
+	}
+
+}
